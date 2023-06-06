b34ba4ab565112d5f03ee4137188b0e2ca3c7042
compli <shirothegamer69@gmail.com>
2023-06-07 02:29:21 +0530

    basic routes added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing/Landing";
+import Members from "./pages/Members/Members";
+import ContactUs from "./pages/Contactus/ContactUs";
+import Aboutus from "./pages/Aboutus/Aboutus";
+import Events from "./pages/Events/Events";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/members" element={<Members />} />
+        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/aboutus" element={<Aboutus />} />
+        <Route path="/events" element={<Events />} />
       </Routes>
     </BrowserRouter>
   );
